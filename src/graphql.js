a765f6f331a54f03189d8f222874ea0933309d67
jsimplicio <hi@julessimplic.io>
2022-01-04 00:41:20 -0600

    Try again

---
+++ src/graphql.js
@@ -0,0 +1,66 @@
+import { ApolloServer, gql } from "apollo-server-lambda";
+
+const typeDefs = gql`
+  type Color {
+    name: String!
+    value: String!
+  }
+
+  type Domain {
+    name: String!
+    color: [Color]!
+  }
+  
+  type Query {
+    domains: [Domain]!
+    domain(name: String!): Domain
+  }
+`;
+
+let firefox = 
+[
+  {
+    name: "Desktop",
+    color: [
+      {
+        name: "Action Primary",
+        value: "rgba(0, 96, 223, 1)"
+      },
+      {
+        name: "Action Primary Hover",
+        value: "rgba(2, 80, 187, 1)"
+      },
+    ]
+  },
+  {
+    name: "Mobile",
+    color: [
+      {
+        name: "Action Primary",
+        value: "a"
+      },
+      {
+        name: "Action Primary Hover",
+        value: "b"
+      },
+    ]
+  },
+]
+
+const resolvers = {
+  Query: {
+    domains: () => domains,
+    domain: (_, args) => {
+      return firefox.find((domain) => domain.name === args.name);
+    }
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  mocks: true,
+  resolvers,
+  playground: true // enable GraphQL Playground IDE on prod env
+});
+
+exports.handler = server.createHandler(); // Don't forget to add 
