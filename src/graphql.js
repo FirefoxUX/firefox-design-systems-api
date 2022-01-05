6fbfd8dd6c68c3641fb529a8da4012e66d5872f0
jsimplicio <hi@julessimplic.io>
2022-01-04 19:49:50 -0600

    Cors

--- src/graphql.js
+++ src/graphql.js
@@ -1,6 +1,32 @@
 require('dotenv').config();
 import { ApolloServer, gql } from "apollo-server-lambda";
 
+
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://studio.apollographql.com"
+  );
+  res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers"
+  );
+  res.setHeader(
+      "Access-Control-Allow-Methods",
+      "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD"
+  );
+  if (req.method === "OPTIONS") {
+      res.end();
+      return false;
+  }
+
+  await startServer;
+  await apolloServer.createHandler({
+      path: "/api/graphql",
+  })(req, res);
+}
+
 const typeDefs = gql`
   type Color {
     name: String!
